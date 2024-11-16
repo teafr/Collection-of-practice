9d9bf47ab78fcac185cb5e0869e0cb8f6d02fc1b
Teafr <tookyoumy7@gmail.com>
2024-11-16 22:00:38 +0200

    added new branch with Current Forecast api project

---
+++ Api Practice/Api Practice/Models/CurrentForecast.cs
@@ -0,0 +1,10 @@
+﻿namespace Api_Practice.Models
+{
+    public class CurrentForecast
+    {
+        public DateTime Time { get; set; }
+        public float Temperature_2m { get; set; }
+        public int Is_day { get; set; }
+        public int Weather_code { get; set; }
+    }
+}
---
+++ Api Practice/Api Practice/Models/KyivForecast.cs
@@ -0,0 +1,9 @@
+﻿namespace Api_Practice.Models
+{
+    public class KyivForecast
+    {
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public CurrentForecast Current { get; set; }
+    }
+}
